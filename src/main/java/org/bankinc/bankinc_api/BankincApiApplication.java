4e96f830cdc4ec8acdbace4f08b3c3cadc1571f1
Carlos Pacanchique <carlospacanchique1904@gmail.com>
2026-03-05 14:49:22 -0500

    commit

--- src/main/java/org/bankinc/bankinc_api/BankincApiApplication.java
+++ src/main/java/org/bankinc/bankinc_api/BankincApiApplication.java
@@ -2,6 +2,10 @@ package org.bankinc.bankinc_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BankincApiApplication {
@@ -9,5 +13,17 @@ public class BankincApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BankincApiApplication.class, args);
 	}
-
+	@Configuration
+	public static class Myconfiguration{
+		@Bean
+		public WebMvcConfigurer corsConfigurer(){
+			return new WebMvcConfigurer() {
+				@Override
+				public void addCorsMappings(CorsRegistry registry) {
+					registry.addMapping("/**")
+							.allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+				}
+			};
+		}
+	}
 }
